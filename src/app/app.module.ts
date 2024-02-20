6cce6871706539aafd2ee280cd0782f07394e3a5
Fabio <fabioec.2010@gmail.com>
2024-02-20 06:11:50 -0400

    Finalizacao dos formularios de login e inicio de rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 import { TemplateModule } from './template/template.module';
 import { HomeComponent } from './home/home.component'
@@ -11,19 +12,25 @@ import { ClientesService } from './clientes.service'
 import { HttpClientModule } from '@Angular/common/http'
 import { ServicoPrestadoModule } from './servico-prestado/servico-prestado.module';
 import { ServicoPrestadoService } from './servico-prestado.service';
+import { LoginComponent } from './login/login.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpClientModule,
     AppRoutingModule,
     TemplateModule,
     ClientesModule,
     ServicoPrestadoModule
+    
   ],
   providers: [
     ClientesService,
